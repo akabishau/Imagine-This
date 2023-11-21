f56cc61823d30f44c2b85402e38758f51e314d36
akabishau <aleksey.kabishau@icloud.com>
2023-11-21 12:26:06 -0500

    Initial Commit

---
+++ ImagineThis/ImagineThisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagineThisApp.swift
+//  ImagineThis
+//
+//  Created by Aleksey Kabishau on 11/21/23.
+//
+
+import SwiftUI
+
+@main
+struct ImagineThisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
