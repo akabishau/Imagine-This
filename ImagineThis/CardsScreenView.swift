f453311a168cfabbf7b96ec3269b124bf8d6697d
akabishau <aleksey.kabishau@icloud.com>
2023-12-06 09:25:45 -0500

    complete swiping cards functionality

--- ImagineThis/CardsScreenView.swift
+++ ImagineThis/CardsScreenView.swift
@@ -7,9 +7,16 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct CardsScreenView: View {
+	@State private var rotations: [Double] = [3, -3, 0]
+	@State private var sentences = ["A brave unicorn is teaching a group of kittens the art of flying.",
+					 "An adventurous penguin is discovering hidden treasures in a mystical forest.",
+					 "A wise owl is solving ancient riddles with a band of friendly ghosts."]
 	
 	@ObservedObject var userSelections: UserSelections
+	@State private var dragOffset = CGSize.zero
 	
 	var onBack: () -> Void
 	
@@ -27,13 +34,41 @@ struct CardsScreenView: View {
 				selectionsView()
 					.padding(.top, 150)
 					.padding(.bottom, 24)
-				Rectangle()
-					.frame(width: 300, height: 180)
+				
+				ZStack {
+					ForEach(Array(sentences.enumerated()), id: \.element) { index, sentence in
+						CardView(sentence: sentence)
+							.rotationEffect(.degrees(rotations[index]))
+							.offset(sentence == sentences.last ? dragOffset : CGSize.zero)
+							.gesture(
+								DragGesture()
+									.onChanged({ gesture in
+										dragOffset = gesture.translation
+									})
+									.onEnded({ _ in
+										withAnimation {
+											moveTopCardToEnd()
+										}
+									})
+							)
+					}
+				}
+				
 				Spacer()
 			}
 			.padding()
 		}
 		.edgesIgnoringSafeArea(.bottom)
+	}
+	
+	private func moveTopCardToEnd() {
+		dragOffset = .zero
+		if let lastSentence = sentences.last, let rotation = rotations.last {
+			rotations.removeLast()
+			sentences.removeLast()
+			rotations.insert(rotation, at: 0)
+			sentences.insert(lastSentence, at: 0)
+		}
 	}
 	
 	@ViewBuilder
@@ -46,11 +81,9 @@ struct CardsScreenView: View {
 		.frame(width: 150)
 	}
 	
-	
 	@ViewBuilder
 	private func selectionsView() -> some View {
 		HStack {
-			
 			Image(userSelections.category.label)
 				.resizable()
 				.scaledToFit()
@@ -60,13 +93,33 @@ struct CardsScreenView: View {
 				.resizable()
 				.scaledToFit()
 				.frame(width: 100)
-			
 		}
 	}
 }
 
-#Preview {
-	CardsScreenView(userSelections: UserSelections()) {
-		print("back button tapped")
+struct CardView: View {
+	let sentence: String
+	
+	var body: some View {
+		ZStack {
+			Image("card")
+				.resizable()
+				.scaledToFit()
+				.shadow(radius: 3)
+			Text(sentence)
+				.multilineTextAlignment(.center)
+				.padding()
+		}
+		.frame(width: 300, height: 200)
 	}
 }
+
+// Preview
+struct CardsScreenView_Previews: PreviewProvider {
+	static var previews: some View {
+		CardsScreenView(userSelections: UserSelections(), onBack: {
+			print("back button tapped")
+		})
+	}
+}
+
